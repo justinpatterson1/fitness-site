356f7e57780f7eda974ae024a3f87e1870705be2
Justin Patterson <justin.patterson17@yahoo.com>
2021-06-30 17:40:31 -0400

    hamburger menu  functionality added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 const hamMenu = document.querySelector(".hamMenu");
-
+const navDiv = document.querySelector("#nav-bar")
+ let open = false;
 hamMenu.addEventListener("click",()=>{
-    alert("hey")
+    if(open === false)
+    {
+        navDiv.style.display = "block";
+        open = true;
+    }
+    else{
+        navDiv.style.display = "none";
+        open = false;
+    }
+    
 })
